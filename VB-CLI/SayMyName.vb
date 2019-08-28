94e6da96c72b8f4820be8a3d6b539a4e03de1dad
Dylan Slack <slacddyla@isu.edu>
2019-08-28 08:42:17 -0600

    SayMyName
    Added Say My Name Assignment

---
+++ VB-CLI/SayMyName.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main()
+	  Console.Beep(440, 50000)
+	  Console.WriteLine("What is your Name?")
+	  Console.Readline()
+	  Console.WriteLine("Hello, ")
+	 End Sub
+End Module
