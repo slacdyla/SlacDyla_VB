4451ea99b758583f17233d96a54532dde9ca5fe6
Dylan Slack <slacddyla@isu.edu>
2019-08-29 08:23:10 -0600

    Finished Say My Name Homework
    Finished Say my Name Homework

--- VB-CLI/SayMyName.vb
+++ VB-CLI/SayMyName.vb
@@ -2,9 +2,11 @@ Imports System
 
 Module Program
     Sub Main()
-	  Console.Beep(440, 2000)
-	  Console.WriteLine("What is your Name?")
-	  Console.Readline()
-	  Console.WriteLine("Hello, ")
-	 End Sub
+        Dim x As String
+
+
+        Console.WriteLine("What is your Name?")
+        x = Console.ReadLine()
+        Console.WriteLine("Hello, " & x)
+    End Sub
 End Module
